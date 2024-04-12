2a72575228844f66a3bc64e23d139d65cfdca9d9
Raghav Sharma <92998665+rs0125@users.noreply.github.com>
2024-04-12 15:52:22 +0530

    Update main.py
    Added Task Inputs, integrated within kanban classes

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import sys
 from termcolor import colored, cprint
 
 class kanban:
-    task_list = []
     def __init__(self, name):
         self.name = name
+        self.task_list = []
 
     def display_tasks(self):
         if len(self.task_list) != 0:
             for i in self.task_list:
                 print()
-                print(i)
+                i.display()
         else:
             print("No Tasks Here")
 
@@ -38,15 +38,34 @@ class Task:
     def add_status_field(self, field_name, field_value):
         setattr(self, field_name, field_value)
 
-def task_input():
+    def display(self):
+        print("Task:", self.task_name)
+        print("Assignee:", self.assignee)
+        print("Reporter:", self.reporter)
+        print("Deadline:", self.deadline)
+        # Display additional status fields
+        for attr_name in dir(self):
+            if not attr_name.startswith('__') and not callable(getattr(self, attr_name)):
+                print(f"{attr_name.capitalize()}: {getattr(self, attr_name)}")
+
+def indiv_input():
     name = input("Enter Task Name")
     assignee = input("Enter Task Assignee")
     reporter = input("Enter Task Reporter")
     deadline = input("Enter Task Deadline")
     priority = input("Enter Task Priority")
+    print()
+    return Task(name, assignee, reporter, deadline, priority)
+
+def kanban_input(Table):
+    Table.task_list.append(indiv_input())
+
 
 todo = kanban("TODO")
 prog = kanban("In Progress")
 done = kanban("Done")
+kanban_input(todo)
+
+# Displaying the task details
 
 kanban_display(todo, prog, done)
